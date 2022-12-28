cfa2fac07f9419c06e1777287250dd3387d6e361
Juan Manuel Aragon Paz <jmanuel@thinkanddev.com>
2022-12-28 10:35:04 -0300

    Update server.js

---
+++ PreEntrega02/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+const app = express();
+const PORT = 8080 || process.env.PORT;
+
+app.use(express.urlenconded({ extended: true }));
+app.use(express.json());
+
+import productosRouter from './productos.js';
+app.use('/api/productos', productosRouter);
+
+import routerCarrito from './carrito.js';
+app.use('/api/carrito', routerCarrito);
+
+app.use('/static', express.static('./src/assets'));
+
+const server = app.listen(PORT, () => {
+    console.log('Servidor de express iniciado');
+});
